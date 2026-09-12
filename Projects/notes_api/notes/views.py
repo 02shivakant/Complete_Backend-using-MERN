d4505374b1dd1e74365f4ddc4063cec49ae28047
dearravan02 <sanjaypandey2334@gmail.com>
2026-09-12 10:38:05 +0530

    DRF done

--- Projects/notes_api/notes/views.py
+++ Projects/notes_api/notes/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets , filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Note
 from .serializers import NoteSerializer
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    filter_backends = [
+        DjangoFilterBackend , 
+        filters.SearchFilter,
+        ]
+    filterset_fields = ['tag']
+
+    search_field = ['title' , 'content' , 'tags']
